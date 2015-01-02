1d5f2854b342e64c76ce18ba3fc3c015ce20a0b9
jihui <jihui@darfoo.com>
2015-01-02 17:25:35 +0800

    为关于视频有关的查询添加了cache

---
+++ app/controllers/VideoController.java
@@ -0,0 +1,89 @@
+package controllers;
+
+import cache.RedisManager;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zjh on 15-1-2.
+ */
+public class VideoController extends Controller {
+    static JedisPool jedisPool = RedisManager.getRedisPoolInstance();
+
+    public static Result singleVideo(Long id){
+        Jedis jedis = null;
+        try {
+            String key = "video-" + id;
+            jedis = jedisPool.getResource();
+            if (!jedis.exists(key)){
+                return redirect("http://localhost:8080/darfoobackend/rest/resources/video/" + id);
+            }else{
+                Map<String, String> result = jedis.hgetAll(key);
+                return ok(Json.toJson(result));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return badRequest("error");
+        }finally {
+            jedisPool.returnResource(jedis);
+        }
+    }
+
+    public static Result indexVideos(){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            if (!jedis.exists("videoindex")){
+                return redirect("http://localhost:8080/darfoobackend/rest/resources/video/cache/index");
+            }else{
+                Set<String> latestVideos = jedis.smembers("videoindex");
+                List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+                for (String vkey : latestVideos){
+                    //System.out.println("vkey -> " + vkey);
+                    Map<String, String> video = jedis.hgetAll(vkey);
+                    //System.out.println("title -> " + video.get("title"));
+                    result.add(video);
+                }
+                return ok(Json.toJson(result));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return badRequest("error");
+        }finally {
+            jedisPool.returnResource(jedis);
+        }
+    }
+
+    public static Result recommendVideos(){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            if (!jedis.exists("videorecommend")){
+                return redirect("http://localhost:8080/darfoobackend/rest/resources/video/cache/recommend");
+            }else{
+                Set<String> latestVideos = jedis.smembers("videorecommend");
+                List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+                for (String vkey : latestVideos){
+                    //System.out.println("vkey -> " + vkey);
+                    Map<String, String> video = jedis.hgetAll(vkey);
+                    //System.out.println("title -> " + video.get("title"));
+                    result.add(video);
+                }
+                return ok(Json.toJson(result));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return badRequest("error");
+        }finally {
+            jedisPool.returnResource(jedis);
+        }
+    }
+}
