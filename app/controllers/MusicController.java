cf1ab3639f39130636eb19b6731f98e9900e1918
jihui <jihui@darfoo.com>
2015-01-03 17:45:43 +0800

    support single music and music by category cache service

--- app/controllers/MusicController.java
+++ app/controllers/MusicController.java
@@ -13,18 +13,18 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Created by zjh on 15-1-2.
+ * Created by zjh on 15-1-3.
  */
-public class VideoController extends Controller {
+public class MusicController extends Controller {
     static JedisPool jedisPool = RedisManager.getRedisPoolInstance();
 
-    public static Result singleVideo(Long id){
+    public static Result singleMusic(Long id){
         Jedis jedis = null;
         try {
-            String key = "video-" + id;
+            String key = "music-" + id;
             jedis = jedisPool.getResource();
             if (!jedis.exists(key)){
-                return redirect("http://localhost:8080/darfoobackend/rest/resources/video/" + id);
+                return redirect("http://localhost:8080/darfoobackend/rest/resources/music/" + id);
             }else{
                 Map<String, String> result = jedis.hgetAll(key);
                 return ok(Json.toJson(result));
@@ -37,7 +37,7 @@ public class VideoController extends Controller {
         }
     }
 
-    public static Result indexVideos(){
+    public static Result hottestMusic(){
         Jedis jedis = null;
         try {
             String key = "videoindex";
@@ -63,39 +63,13 @@ public class VideoController extends Controller {
         }
     }
 
-    public static Result recommendVideos(){
-        Jedis jedis = null;
-        try {
-            String key = "videorecommend";
-            jedis = jedisPool.getResource();
-            if (!jedis.exists(key)){
-                return redirect("http://localhost:8080/darfoobackend/rest/cache/video/recommend");
-            }else{
-                Set<String> latestVideos = jedis.smembers(key);
-                List<Map<String, String>> result = new ArrayList<Map<String, String>>();
-                for (String vkey : latestVideos){
-                    //System.out.println("vkey -> " + vkey);
-                    Map<String, String> video = jedis.hgetAll(vkey);
-                    //System.out.println("title -> " + video.get("title"));
-                    result.add(video);
-                }
-                return ok(Json.toJson(result));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            return badRequest("error");
-        }finally {
-            jedisPool.returnResource(jedis);
-        }
-    }
-
     public static Result category(String category){
         Jedis jedis = null;
         try {
-            String key = "videocategory" + category;
+            String key = "musiccategory" + category;
             jedis = jedisPool.getResource();
             if (!jedis.exists(key)){
-                return redirect("http://localhost:8080/darfoobackend/rest/cache/video/category/" + category);
+                return redirect("http://localhost:8080/darfoobackend/rest/cache/music/category/" + category);
             }else{
                 Set<String> latestVideos = jedis.smembers(key);
                 List<Map<String, String>> result = new ArrayList<Map<String, String>>();
@@ -105,26 +79,6 @@ public class VideoController extends Controller {
                     //System.out.println("title -> " + video.get("title"));
                     result.add(video);
                 }
-                return ok(Json.toJson(result));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            return badRequest("error");
-        }finally {
-            jedisPool.returnResource(jedis);
-        }
-    }
-
-    public static Result getMusic(Long id){
-        Jedis jedis = null;
-        try {
-            String key = "videomusic-" + id;
-            jedis = jedisPool.getResource();
-            if (!jedis.exists(key)){
-                return redirect("http://localhost:8080/darfoobackend/rest/cache/video/getmusic/" + id);
-            }else{
-                String mkey = "music-" + jedis.get(key);
-                Map<String, String> result = jedis.hgetAll(mkey);
                 return ok(Json.toJson(result));
             }
         }catch (Exception e){
