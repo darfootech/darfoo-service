68ee2c0df8b5fbd1e6c1b3b43e99bb4b66cb5e9a
jihui <jihui@darfoo.com>
2015-01-08 15:13:18 +0800

    finish insertorupdate tab and resource

--- test/StatisticTest.java
+++ test/StatisticTest.java
@@ -73,6 +73,27 @@ public class StatisticTest {
     }
 
     @Test
+    public void insertOrUpdateTab(){
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                String macaddress = "00:ad:05:01:a6:85";
+                String hostip = "fe80::2ad:5ff:fe01:a685%wlan0";
+                int tabid = 3;
+                Tab tab = Ebean.find(Tab.class).where().eq("mac", macaddress).eq("hostip", hostip).eq("tabid", tabid).findUnique();
+
+                if (tab != null){
+                    tab.clickcount += 1;
+                    tab.update();
+                }else{
+                    Tab newTab = new Tab(macaddress, hostip, tabid, 0L);
+                    newTab.save();
+                }
+            }
+        });
+    }
+
+    @Test
     public void insertTabTime(){
         running(fakeApplication(), new Runnable() {
             @Override
@@ -97,6 +118,28 @@ public class StatisticTest {
                 int resourceid = 333;
                 Resource resource = new Resource(macaddress, hostip, resourcetype, resourceid, 0L);
                 resource.save();
+            }
+        });
+    }
+
+    @Test
+    public void insertOrUpdateResource(){
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                String macaddress = "00:ad:05:01:a6:85";
+                String hostip = "fe80::2ad:5ff:fe01:a685%wlan0";
+                String resourcetype = "video";
+                int resourceid = 3;
+                Resource resource = Ebean.find(Resource.class).where().eq("mac", macaddress).eq("hostip", hostip).eq("type", resourcetype).eq("resourceid", resourceid).findUnique();
+
+                if (resource != null){
+                    resource.clickcount += 1;
+                    resource.update();
+                }else{
+                    Resource newResource = new Resource(macaddress, hostip, resourcetype, resourceid, 0L);
+                    newResource.save();
+                }
             }
         });
     }
