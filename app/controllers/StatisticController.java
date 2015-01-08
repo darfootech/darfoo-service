1ad66167061795730b67902a08b7c2dd903a48c1
jihui <jihui@darfoo.com>
2015-01-08 21:39:06 +0800

    add statistic sample url

--- app/controllers/StatisticController.java
+++ app/controllers/StatisticController.java
@@ -10,21 +10,25 @@ import utils.StatisticUtils;
 public class StatisticController extends Controller {
 
     /*for resource statistics*/
+    //http://localhost:9000/statistics/video/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result singleVideo(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateResource(mac, host, "video", id);
         return ok();
     }
 
+    //http://localhost:9000/statistics/tutorial/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result singleTutorial(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateResource(mac, host, "tutorial", id);
         return ok();
     }
 
+    //http://localhost:9000/statistics/music/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result singleMusic(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateResource(mac, host, "music", id);
         return ok();
     }
 
+    //http://localhost:9000/statistics/author/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result singleAuthor(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateResource(mac, host, "author", id);
         return ok();
@@ -32,6 +36,7 @@ public class StatisticController extends Controller {
     /*end of resource statistics*/
 
     /*for menu statistics*/
+    //http://localhost:9000/statistics/menu/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result menu(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateMenu(mac, host, id);
         return ok();
@@ -39,6 +44,7 @@ public class StatisticController extends Controller {
     /*end of menu statistics*/
 
     /*for tab statistics*/
+    //http://localhost:9000/statistics/tab/3/m/00:ad:05:01:a6:85/h/fe80::2ad:5ff:fe01:a685-wlan0
     public static Result tab(Integer id, String mac, String host){
         StatisticUtils.insertOrUpdateTab(mac, host, id);
         return ok();
