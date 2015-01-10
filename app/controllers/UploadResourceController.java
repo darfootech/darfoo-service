351f0783bdf5f63fb572729c9e4e2fd7f62bcf62
jihui <jihui@darfoo.com>
2015-01-10 18:15:47 +0800

    上传视频的两个service也加上了auth

--- app/controllers/UploadResourceController.java
+++ app/controllers/UploadResourceController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import models.auth.User;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.libs.Json;
@@ -17,19 +19,48 @@ import java.util.Map;
 public class UploadResourceController extends Controller {
 
     public static Result getUploadToken(){
-        String token = QiniuUtils.getToken();
-        System.out.println("origin token -> " + token);
-        String encryptToken = CryptUtils.base64EncodeStr(token);
         Map<String, Object> result = new HashMap<String, Object>();
-        result.put("tk", encryptToken);
-        return ok(Json.toJson(result));
+
+        DynamicForm form = Form.form().bindFromRequest();
+        String username = form.get("username");
+        String password = form.get("password");
+
+        User user = Ebean.find(User.class).where().eq("username", username).eq("password", password).findUnique();
+        if (user != null){
+            System.out.println("用户已经存在");
+            String token = QiniuUtils.getToken();
+            System.out.println("origin token -> " + token);
+            String encryptToken = CryptUtils.base64EncodeStr(token);
+            result.put("tk", encryptToken);
+            return ok(Json.toJson(result));
+        }else{
+            System.out.println("用户不存在");
+            result.put("tk", "error");
+            return ok(Json.toJson(result));
+        }
     }
 
     public static Result uploadFinishCallback(){
-        DynamicForm form = Form.form().bindFromRequest();
-        System.out.println("title -> " + form.get("title"));
-        System.out.println("authorid -> " + form.get("authorid"));
+        Map<String, Object> result = new HashMap<String, Object>();
 
-        return ok("ok");
+        DynamicForm form = Form.form().bindFromRequest();
+        String username = form.get("username");
+        String password = form.get("password");
+        String videokey = form.get("videokey");
+
+        System.out.println("username -> " + username);
+        System.out.println("password -> " + password);
+        System.out.println("videokey -> " + videokey);
+
+        User user = Ebean.find(User.class).where().eq("username", username).eq("password", password).findUnique();
+        if (user != null){
+            System.out.println("用户已经存在");
+            result.put("status", "ok");
+            return ok(Json.toJson(result));
+        }else{
+            System.out.println("用户不存在");
+            result.put("status", "error");
+            return ok(Json.toJson(result));
+        }
     }
 }
