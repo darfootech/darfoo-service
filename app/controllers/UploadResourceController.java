bdc60dd50eb9c55c044fd3dedec2bafdb2bd4e82
jihui <jihui@darfoo.com>
2015-01-11 14:11:21 +0800

    add prepare upload service

--- app/controllers/UploadResourceController.java
+++ app/controllers/UploadResourceController.java
@@ -48,6 +48,16 @@ public class UploadResourceController extends Controller {
         }
     }
 
+    /**
+     * launcher上传视频之前要先确定上传的video标识也就是名字加后缀是否已经存在了
+     * @return
+     */
+    public static Result prepareUpload(String videokey){
+        String requestUrl = baseUrl + "/uploadresource/prepareupload/" + videokey;
+        String response = new HttpUtils().getRequest(requestUrl);
+        return ok(response);
+    }
+
     public static Result uploadFinishCallback(){
         Map<String, Object> result = new HashMap<String, Object>();
 
