62f4daf4568a9520c16265dddc697336e37d6d94
jihui <jihui@darfoo.com>
2015-02-08 21:21:44 +0800

    修改了服务器权限之后升级数据库备份脚本

--- databasebackup.sh
+++ databasebackup.sh
@@ -1,4 +1,4 @@
-ssh root@115.29.238.12   'cd /home/darfoodatabase; \
+ssh -p 33333 service@115.29.238.12   'cd /home/service/darfoodatabase; \
                           mysqldump --extended-insert=FALSE --complete-insert=TRUE -uroot -p darfoo > darfoo.sql; \
                           git commit -a -m"backupdatabase";\
                           git push'
